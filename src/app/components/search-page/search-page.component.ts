acfb5960662a3388e254ebb486fa01526dccdb24
FazioNico <contact@nicolasfazio.ch>
2020-06-25 01:24:18 +0200

    upd: search ui + item ui

--- src/app/components/search-page/search-page.component.ts
+++ src/app/components/search-page/search-page.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { AdditivesService } from 'src/app/services/additives/additives.service';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search-page',
   templateUrl: './search-page.component.html',
-  styleUrls: ['./search-page.component.scss']
+  styleUrls: ['./search-page.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class SearchPageComponent {
 
