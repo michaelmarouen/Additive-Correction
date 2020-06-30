9086f1386b4d1b1ab09749f6f35f0d9c38c09fa2
MMC <michaelconfortini@gmail.com>
2020-06-30 09:36:15 +0200

    deploy on githb

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { IonicModule } from '@ionic/angular';
-import { TabsComponent } from './components/tabs/tabs.component';
-import { SearchPageComponent } from './components/search-page/search-page.component';
-import { ListPageComponent } from './components/list-page/list-page.component';
-import { ItemPageComponent } from './components/item-page/item-page.component';
 import { HttpClientModule } from '@angular/common/http';
 import { LevelPipe } from './pipes/level/level.pipe';
 import { ColorPipe } from './pipes/color/color.pipe';
 import { ServiceWorkerModule } from '@angular/service-worker';
+
+import { IonicModule } from '@ionic/angular';
+
 import { environment } from '../environments/environment';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { TabsComponent } from './components/tabs/tabs.component';
+import { SearchPageComponent } from './components/search-page/search-page.component';
+import { ListPageComponent } from './components/list-page/list-page.component';
+import { ItemPageComponent } from './components/item-page/item-page.component';
 import { UpdatesNotificationComponent } from './components/updates-notification/updates-notification.component';
-import { ToastComponent } from './shared/toast/toast/toast.component';
+
 
 @NgModule({
   declarations: [
@@ -26,14 +29,13 @@ import { ToastComponent } from './shared/toast/toast/toast.component';
     ItemPageComponent,
     LevelPipe,
     ColorPipe,
-    UpdatesNotificationComponent,
-    ToastComponent
+    UpdatesNotificationComponent
   ],
   imports: [
+    AppRoutingModule,
+    BrowserModule,
     FormsModule,
     HttpClientModule,
-    BrowserModule,
-    AppRoutingModule,
     IonicModule.forRoot(),
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
